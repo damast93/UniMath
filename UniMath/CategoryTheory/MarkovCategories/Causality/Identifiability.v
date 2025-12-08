fba7d872b3d660ef770e6a30b6581465902f52e1
Dario Stein <damast93@aol.com>
2025-12-08 21:20:46 +0100

    Stated back door criterion

--- UniMath/CategoryTheory/MarkovCategories/Causality/Identifiability.v
+++ UniMath/CategoryTheory/MarkovCategories/Causality/Identifiability.v
@@ -23,12 +23,25 @@ Require Import UniMath.CategoryTheory.Monoidal.Structure.Symmetric.
 
 Require Import UniMath.CategoryTheory.MarkovCategories.MarkovCategory.
 Require Import UniMath.CategoryTheory.MarkovCategories.Determinism.
+Require Import UniMath.CategoryTheory.MarkovCategories.Univalence.
+
 Require Import UniMath.CategoryTheory.MarkovCategories.State.
 Require Import UniMath.CategoryTheory.MarkovCategories.InformationFlowAxioms.
 Require Import UniMath.CategoryTheory.MarkovCategories.AlmostSurely.
 Require Import UniMath.CategoryTheory.MarkovCategories.Conditionals.
-Require Import UniMath.CategoryTheory.MarkovCategories.Couplings.
 
+Require Import UniMath.CategoryTheory.DaggerCategories.Categories.
+Require Import UniMath.CategoryTheory.DaggerCategories.Unitary.
+Require Import UniMath.CategoryTheory.DaggerCategories.Univalence.
+
+Import MonoidalNotations.
+
+Local Open Scope cat.
+Local Open Scope moncat.
+Local Open Scope markov.
+
+Definition full_support {C : markov_category} {x : C} (p : I_{C} --> x) : UU
+  := ∏ (y : C) (f g : x --> y), (f =_{p} g) -> f = g.  
 
 Section BackdoorExample.
   Context {C : markov_category_with_conditionals}.
@@ -44,5 +57,54 @@ X --> Y
 
 we can compute the causal effect P(Y|do(X)) by adjusting on Z.
 *)
+
+  Context (x y z : C).
+
+  Definition causal_model : UU := 
+    (I_{C} --> z) × (z --> x) × (x ⊗ z --> y).
+
+  Definition y_do_x (m : causal_model) : x --> y.
+  Proof.
+    destruct m as (pz & px & py).
+    refine (mon_rinvunitor _ · (identity x #⊗ pz) · py).
+  Defined.
+
+  Definition joint (m : causal_model) : I_{C} --> x ⊗ z ⊗ y.
+  Proof.
+    destruct m as (pz & px & py).
+    exact (pz · ⟨px, identity z⟩ · ⟨identity _, py⟩).
+  Defined.  
+
+  Proposition y_do_x_identifiable1 
+    (m1 m2 : causal_model) 
+    (e : joint m1 = joint m2)
+    :  y_do_x m1 = y_do_x m2.
+  Proof.
+  Admitted.
+
+  (* support fn *)
+  Definition y_do_x_compute
+    (pxzy : I_{C} --> x ⊗ z ⊗ y)
+    : x --> y.
+  Proof.
+  Admitted.
+
+  (* exists fn, ... *)
+
+  Proposition y_do_x_identifiable2 
+    (m : causal_model)
+    : y_do_x_compute (joint m) = y_do_x m.
+  Proof.
+  Admitted.
+
+  
+  Proposition y_do_x_identifiable3 
+    (pxzy : I_{C} --> x ⊗ z ⊗ y)
+    (m : causal_model)
+    (e : pxzy = joint m)
+    (ff : full_support pxzy)
+    : y_do_x_compute pxzy = y_do_x m.
+  Proof.
+  Admitted.
 
 End BackdoorExample.
